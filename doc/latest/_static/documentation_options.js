4d3bda148eb78d7d0a5aa6180a65bcd472dcc75c
lief-ci-doc <lief@quarkslab.com>
2021-05-22 09:22:38 +0000

    Update latest doc

--- doc/latest/_static/documentation_options.js
+++ doc/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.0-92dac82',
+    VERSION: '0.12.0-3d4799a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
