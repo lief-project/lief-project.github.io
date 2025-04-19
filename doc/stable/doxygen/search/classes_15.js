d8fac9a2aee29498462099a6ca142de41ee8c428
lief-ci-doc <lief-ci-doc@lief.re>
2025-04-19 17:33:54 +0000

    Update latest doc

--- doc/stable/doxygen/search/classes_15.js
+++ doc/stable/doxygen/search/classes_15.js
@@ -4,5 +4,6 @@ var searchData=
   ['vector_5fiostream_1',['vector_iostream',['../classLIEF_1_1vector__iostream.html',1,'LIEF']]],
   ['vectorstream_2',['VectorStream',['../classLIEF_1_1VectorStream.html',1,'LIEF']]],
   ['versionmin_3',['VersionMin',['../classLIEF_1_1MachO_1_1VersionMin.html',1,'LIEF::MachO']]],
-  ['visitor_4',['Visitor',['../classLIEF_1_1Visitor.html',1,'LIEF']]]
+  ['visitor_4',['Visitor',['../classLIEF_1_1Visitor.html',1,'LIEF']]],
+  ['volatile_5',['Volatile',['../classLIEF_1_1dwarf_1_1types_1_1Volatile.html',1,'LIEF::dwarf::types']]]
 ];
--- doc/stable/doxygen/search/enums_11.js
+++ doc/stable/doxygen/search/enums_11.js
@@ -3,7 +3,7 @@ var searchData=
   ['va_5ftypes_0',['VA_TYPES',['../classLIEF_1_1Binary.html#a63897a975536c5359d2b3ec82208fab3',1,'LIEF::Binary']]],
   ['verification_5fchecks_1',['VERIFICATION_CHECKS',['../classLIEF_1_1PE_1_1Signature.html#abf70940910955c531f8d11e2fd374bdb',1,'LIEF::PE::Signature']]],
   ['verification_5fflags_2',['VERIFICATION_FLAGS',['../classLIEF_1_1PE_1_1Signature.html#aefd5b1e4458abd33e7993d37846f7d0c',1,'LIEF::PE::Signature::VERIFICATION_FLAGS'],['../classLIEF_1_1PE_1_1x509.html#ac888ffa531a038404ce0477399c407f9',1,'LIEF::PE::x509::VERIFICATION_FLAGS']]],
-  ['version_3',['VERSION',['../classLIEF_1_1ELF_1_1Header.html#a6b0aae41354f900f39f6a9f7f01c157f',1,'LIEF::ELF::Header::VERSION'],['../classLIEF_1_1PE_1_1LoadConfiguration.html#af12fdb35010fc872f805e0594a332a32',1,'LIEF::PE::LoadConfiguration::VERSION']]],
+  ['version_3',['VERSION',['../classLIEF_1_1dsc_1_1DyldSharedCache.html#a8105805c5456d01853167c3bbb9887e5',1,'LIEF::dsc::DyldSharedCache::VERSION'],['../classLIEF_1_1ELF_1_1Header.html#a6b0aae41354f900f39f6a9f7f01c157f',1,'LIEF::ELF::Header::VERSION'],['../classLIEF_1_1PE_1_1LoadConfiguration.html#af12fdb35010fc872f805e0594a332a32',1,'LIEF::PE::LoadConfiguration::VERSION']]],
   ['visibility_4',['VISIBILITY',['../classLIEF_1_1ELF_1_1Symbol.html#afd4269ee33474c4d97489c68c668aaf5',1,'LIEF::ELF::Symbol']]],
   ['vm_5fprotections_5',['VM_PROTECTIONS',['../classLIEF_1_1MachO_1_1SegmentCommand.html#a7e67b23821e8516801981eb147995419',1,'LIEF::MachO::SegmentCommand']]]
 ];
