68009ebde61c53ab4b2a5ae50e6880c2a6cfb524
lief-ci-doc <lief-ci-doc@lief.re>
2025-05-10 04:22:23 +0000

    Update latest doc

--- doc/latest/doxygen/search/classes_1.js
+++ doc/latest/doxygen/search/classes_1.js
@@ -9,7 +9,9 @@ var searchData=
   ['bindinginfoiterator_6',['BindingInfoIterator',['../classLIEF_1_1MachO_1_1BindingInfoIterator.html',1,'LIEF::MachO']]],
   ['bitfield_7',['BitField',['../classLIEF_1_1pdb_1_1types_1_1BitField.html',1,'LIEF::pdb::types']]],
   ['brooks_5fread_5fbarrier_5ft_8',['brooks_read_barrier_t',['../structLIEF_1_1ART_1_1details_1_1ART__17_1_1Java_1_1brooks__read__barrier__t.html',1,'LIEF::ART::details::ART_17::Java']]],
-  ['builder_9',['Builder',['../classLIEF_1_1ELF_1_1Builder.html',1,'LIEF::ELF::Builder'],['../classLIEF_1_1MachO_1_1Builder.html',1,'LIEF::MachO::Builder'],['../classLIEF_1_1PE_1_1Builder.html',1,'LIEF::PE::Builder']]],
-  ['buildtoolversion_10',['BuildToolVersion',['../classLIEF_1_1MachO_1_1BuildToolVersion.html',1,'LIEF::MachO']]],
-  ['buildversion_11',['BuildVersion',['../classLIEF_1_1MachO_1_1BuildVersion.html',1,'LIEF::MachO']]]
+  ['build_5finfo_5ft_9',['build_info_t',['../structLIEF_1_1pdb_1_1BuildMetadata_1_1build__info__t.html',1,'LIEF::pdb::BuildMetadata']]],
+  ['builder_10',['Builder',['../classLIEF_1_1ELF_1_1Builder.html',1,'LIEF::ELF::Builder'],['../classLIEF_1_1MachO_1_1Builder.html',1,'LIEF::MachO::Builder'],['../classLIEF_1_1PE_1_1Builder.html',1,'LIEF::PE::Builder']]],
+  ['buildmetadata_11',['BuildMetadata',['../classLIEF_1_1pdb_1_1BuildMetadata.html',1,'LIEF::pdb']]],
+  ['buildtoolversion_12',['BuildToolVersion',['../classLIEF_1_1MachO_1_1BuildToolVersion.html',1,'LIEF::MachO']]],
+  ['buildversion_13',['BuildVersion',['../classLIEF_1_1MachO_1_1BuildVersion.html',1,'LIEF::MachO']]]
 ];
--- doc/stable/doxygen/search/files_12.js
+++ doc/stable/doxygen/search/files_12.js
@@ -1,9 +1,12 @@
 var searchData=
 [
-  ['threadcommand_2ehpp_0',['ThreadCommand.hpp',['../ThreadCommand_8hpp.html',1,'']]],
-  ['tls_2ehpp_1',['TLS.hpp',['../TLS_8hpp.html',1,'']]],
-  ['to_5fjson_2ehpp_2',['to_json.hpp',['../to__json_8hpp.html',1,'']]],
-  ['twolevelhints_2ehpp_3',['TwoLevelHints.hpp',['../TwoLevelHints_8hpp.html',1,'']]],
-  ['types_2eh_4',['types.h',['../types_8h.html',1,'']]],
-  ['types_2ehpp_5',['types.hpp',['../types_8hpp.html',1,'']]]
+  ['templatealias_2ehpp_0',['TemplateAlias.hpp',['../TemplateAlias_8hpp.html',1,'']]],
+  ['threadcommand_2ehpp_1',['ThreadCommand.hpp',['../ThreadCommand_8hpp.html',1,'']]],
+  ['thrown_2ehpp_2',['Thrown.hpp',['../Thrown_8hpp.html',1,'']]],
+  ['tls_2ehpp_3',['TLS.hpp',['../TLS_8hpp.html',1,'']]],
+  ['to_5fjson_2ehpp_4',['to_json.hpp',['../to__json_8hpp.html',1,'']]],
+  ['twolevelhints_2ehpp_5',['TwoLevelHints.hpp',['../TwoLevelHints_8hpp.html',1,'']]],
+  ['typedef_2ehpp_6',['Typedef.hpp',['../Typedef_8hpp.html',1,'']]],
+  ['types_2eh_7',['types.h',['../types_8h.html',1,'']]],
+  ['types_2ehpp_8',['types.hpp',['../types_8hpp.html',1,'']]]
 ];
