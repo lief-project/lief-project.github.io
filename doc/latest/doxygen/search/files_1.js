cbcfec24c67a2ed15d0fac3fb25d5bb3c0483ede
lief-ci-doc <lief-ci-doc@lief.re>
2025-05-30 09:12:29 +0000

    Update latest doc

--- doc/latest/doxygen/search/files_1.js
+++ doc/latest/doxygen/search/files_1.js
@@ -1,12 +1,14 @@
 var searchData=
 [
   ['base_2ehpp_0',['Base.hpp',['../Base_8hpp.html',1,'']]],
-  ['binaryparser_2ehpp_1',['BinaryParser.hpp',['../BinaryParser_8hpp.html',1,'']]],
-  ['binarystream_2ehpp_2',['BinaryStream.hpp',['../BinaryStream_8hpp.html',1,'']]],
-  ['bindinginfo_2ehpp_3',['BindingInfo.hpp',['../BindingInfo_8hpp.html',1,'']]],
-  ['bindinginfoiterator_2ehpp_4',['BindingInfoIterator.hpp',['../BindingInfoIterator_8hpp.html',1,'']]],
-  ['bitfield_2ehpp_5',['BitField.hpp',['../BitField_8hpp.html',1,'']]],
-  ['bpf_2edef_6',['BPF.def',['../BPF_8def.html',1,'']]],
-  ['buildtoolversion_2ehpp_7',['BuildToolVersion.hpp',['../BuildToolVersion_8hpp.html',1,'']]],
-  ['buildversion_2ehpp_8',['BuildVersion.hpp',['../BuildVersion_8hpp.html',1,'']]]
+  ['basetype_2ehpp_1',['BaseType.hpp',['../BaseType_8hpp.html',1,'']]],
+  ['binaryparser_2ehpp_2',['BinaryParser.hpp',['../BinaryParser_8hpp.html',1,'']]],
+  ['binarystream_2ehpp_3',['BinaryStream.hpp',['../BinaryStream_8hpp.html',1,'']]],
+  ['bindinginfo_2ehpp_4',['BindingInfo.hpp',['../BindingInfo_8hpp.html',1,'']]],
+  ['bindinginfoiterator_2ehpp_5',['BindingInfoIterator.hpp',['../BindingInfoIterator_8hpp.html',1,'']]],
+  ['bitfield_2ehpp_6',['BitField.hpp',['../BitField_8hpp.html',1,'']]],
+  ['bpf_2edef_7',['BPF.def',['../BPF_8def.html',1,'']]],
+  ['buildmetadata_2ehpp_8',['BuildMetadata.hpp',['../BuildMetadata_8hpp.html',1,'']]],
+  ['buildtoolversion_2ehpp_9',['BuildToolVersion.hpp',['../BuildToolVersion_8hpp.html',1,'']]],
+  ['buildversion_2ehpp_10',['BuildVersion.hpp',['../BuildVersion_8hpp.html',1,'']]]
 ];
---
+++ doc/stable/_sources/extended/disassembler/cpp/arch/ebpf.rst.txt
@@ -0,0 +1,12 @@
+:fa:`solid fa-microchip` eBPF
+-----------------------------
+
+Instruction
+************
+
+.. doxygenclass:: LIEF::assembly::ebpf::Instruction
+
+Opcodes
+*******
+
+See ``LIEF::assembly::ebpf::OPCODE`` in ``include/asm/ebpf/opcodes.hpp``
