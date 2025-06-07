213e1b55d7da5bf82f59076d2b62b2b73353b2be
lief-ci-doc <lief-ci-doc@lief.re>
2025-06-07 10:06:38 +0000

    Update latest doc

--- doc/latest/doxygen/search/classes_1.js
+++ doc/latest/doxygen/search/classes_1.js
@@ -1,15 +1,18 @@
 var searchData=
 [
   ['base_0',['Base',['../classLIEF_1_1dwarf_1_1types_1_1Base.html',1,'LIEF::dwarf::types']]],
-  ['binary_1',['Binary',['../classLIEF_1_1Binary.html',1,'LIEF::Binary'],['../classLIEF_1_1ELF_1_1Binary.html',1,'LIEF::ELF::Binary'],['../classLIEF_1_1MachO_1_1Binary.html',1,'LIEF::MachO::Binary'],['../classLIEF_1_1OAT_1_1Binary.html',1,'LIEF::OAT::Binary'],['../classLIEF_1_1PE_1_1Binary.html',1,'LIEF::PE::Binary']]],
-  ['binaryparser_2',['BinaryParser',['../classLIEF_1_1MachO_1_1BinaryParser.html',1,'LIEF::MachO']]],
-  ['binarystream_3',['BinaryStream',['../classLIEF_1_1BinaryStream.html',1,'LIEF']]],
-  ['binding_5finfo_5ft_4',['binding_info_t',['../structLIEF_1_1MachO_1_1DyldChainedFixupsCreator_1_1binding__info__t.html',1,'LIEF::MachO::DyldChainedFixupsCreator']]],
-  ['bindinginfo_5',['BindingInfo',['../classLIEF_1_1MachO_1_1BindingInfo.html',1,'LIEF::MachO']]],
-  ['bindinginfoiterator_6',['BindingInfoIterator',['../classLIEF_1_1MachO_1_1BindingInfoIterator.html',1,'LIEF::MachO']]],
-  ['bitfield_7',['BitField',['../classLIEF_1_1pdb_1_1types_1_1BitField.html',1,'LIEF::pdb::types']]],
-  ['brooks_5fread_5fbarrier_5ft_8',['brooks_read_barrier_t',['../structLIEF_1_1ART_1_1details_1_1ART__17_1_1Java_1_1brooks__read__barrier__t.html',1,'LIEF::ART::details::ART_17::Java']]],
-  ['builder_9',['Builder',['../classLIEF_1_1ELF_1_1Builder.html',1,'LIEF::ELF::Builder'],['../classLIEF_1_1MachO_1_1Builder.html',1,'LIEF::MachO::Builder'],['../classLIEF_1_1PE_1_1Builder.html',1,'LIEF::PE::Builder']]],
-  ['buildtoolversion_10',['BuildToolVersion',['../classLIEF_1_1MachO_1_1BuildToolVersion.html',1,'LIEF::MachO']]],
-  ['buildversion_11',['BuildVersion',['../classLIEF_1_1MachO_1_1BuildVersion.html',1,'LIEF::MachO']]]
+  ['basetype_1',['BaseType',['../classLIEF_1_1dwarf_1_1editor_1_1BaseType.html',1,'LIEF::dwarf::editor']]],
+  ['binary_2',['Binary',['../classLIEF_1_1Binary.html',1,'LIEF::Binary'],['../classLIEF_1_1ELF_1_1Binary.html',1,'LIEF::ELF::Binary'],['../classLIEF_1_1MachO_1_1Binary.html',1,'LIEF::MachO::Binary'],['../classLIEF_1_1OAT_1_1Binary.html',1,'LIEF::OAT::Binary'],['../classLIEF_1_1PE_1_1Binary.html',1,'LIEF::PE::Binary']]],
+  ['binaryparser_3',['BinaryParser',['../classLIEF_1_1MachO_1_1BinaryParser.html',1,'LIEF::MachO']]],
+  ['binarystream_4',['BinaryStream',['../classLIEF_1_1BinaryStream.html',1,'LIEF']]],
+  ['binding_5finfo_5ft_5',['binding_info_t',['../structLIEF_1_1MachO_1_1DyldChainedFixupsCreator_1_1binding__info__t.html',1,'LIEF::MachO::DyldChainedFixupsCreator']]],
+  ['bindinginfo_6',['BindingInfo',['../classLIEF_1_1MachO_1_1BindingInfo.html',1,'LIEF::MachO']]],
+  ['bindinginfoiterator_7',['BindingInfoIterator',['../classLIEF_1_1MachO_1_1BindingInfoIterator.html',1,'LIEF::MachO']]],
+  ['bitfield_8',['BitField',['../classLIEF_1_1pdb_1_1types_1_1BitField.html',1,'LIEF::pdb::types']]],
+  ['brooks_5fread_5fbarrier_5ft_9',['brooks_read_barrier_t',['../structLIEF_1_1ART_1_1details_1_1ART__17_1_1Java_1_1brooks__read__barrier__t.html',1,'LIEF::ART::details::ART_17::Java']]],
+  ['build_5finfo_5ft_10',['build_info_t',['../structLIEF_1_1pdb_1_1BuildMetadata_1_1build__info__t.html',1,'LIEF::pdb::BuildMetadata']]],
+  ['builder_11',['Builder',['../classLIEF_1_1ELF_1_1Builder.html',1,'LIEF::ELF::Builder'],['../classLIEF_1_1MachO_1_1Builder.html',1,'LIEF::MachO::Builder'],['../classLIEF_1_1PE_1_1Builder.html',1,'LIEF::PE::Builder']]],
+  ['buildmetadata_12',['BuildMetadata',['../classLIEF_1_1pdb_1_1BuildMetadata.html',1,'LIEF::pdb']]],
+  ['buildtoolversion_13',['BuildToolVersion',['../classLIEF_1_1MachO_1_1BuildToolVersion.html',1,'LIEF::MachO']]],
+  ['buildversion_14',['BuildVersion',['../classLIEF_1_1MachO_1_1BuildVersion.html',1,'LIEF::MachO']]]
 ];
--- doc/latest/doxygen/search/enums_2.js
+++ doc/latest/doxygen/search/enums_2.js
@@ -1,9 +1,11 @@
 var searchData=
 [
   ['category_0',['CATEGORY',['../classLIEF_1_1MachO_1_1Symbol.html#a650b1fdb5a063ecc4ac5d659e9a4a1f8',1,'LIEF::MachO::Symbol']]],
-  ['characteristics_1',['CHARACTERISTICS',['../classLIEF_1_1PE_1_1Header.html#a08d38528d74f0baf71291de383c447fd',1,'LIEF::PE::Header::CHARACTERISTICS'],['../classLIEF_1_1PE_1_1Section.html#a802580e157009845c3859c02554bc6be',1,'LIEF::PE::Section::CHARACTERISTICS']]],
+  ['characteristics_1',['CHARACTERISTICS',['../classLIEF_1_1PE_1_1AuxiliaryWeakExternal.html#a6a19067b6da618a4a31a9dfa62959435',1,'LIEF::PE::AuxiliaryWeakExternal::CHARACTERISTICS'],['../classLIEF_1_1PE_1_1ExDllCharacteristics.html#a31d41be37affe9ca2c434dc03ef250c9',1,'LIEF::PE::ExDllCharacteristics::CHARACTERISTICS'],['../classLIEF_1_1PE_1_1Header.html#a08d38528d74f0baf71291de383c447fd',1,'LIEF::PE::Header::CHARACTERISTICS'],['../classLIEF_1_1PE_1_1Section.html#a802580e157009845c3859c02554bc6be',1,'LIEF::PE::Section::CHARACTERISTICS']]],
   ['class_2',['CLASS',['../classLIEF_1_1ELF_1_1Header.html#ae80a4fad99408ced3a33b1b0733bf8f2',1,'LIEF::ELF::Header::CLASS'],['../classLIEF_1_1MachO_1_1DyldBindingInfo.html#a6ee4075ffe69d2eac8348732225baa62',1,'LIEF::MachO::DyldBindingInfo::CLASS']]],
-  ['code_5fpages_3',['CODE_PAGES',['../namespaceLIEF_1_1PE.html#acf3dbfe6d56bca6be4285a3cb319bcbe',1,'LIEF::PE']]],
-  ['codeviewidentifiers_4',['CodeViewIdentifiers',['../namespaceLIEF_1_1PE.html#a3fa0562bb7025c2f79da8a8aa14bc2ba',1,'LIEF::PE']]],
-  ['cpu_5ftype_5',['CPU_TYPE',['../classLIEF_1_1MachO_1_1Header.html#ac6ba4ef6166df7dbfb54dcfee3157332',1,'LIEF::MachO::Header']]]
+  ['code_5fpages_3',['CODE_PAGES',['../namespaceLIEF_1_1PE.html#a2ce7314545a4dd7624f0c59add53f8d0',1,'LIEF::PE']]],
+  ['complex_5ftype_4',['COMPLEX_TYPE',['../classLIEF_1_1PE_1_1Symbol.html#a46e20207be1426efbe6f2003b527e21d',1,'LIEF::PE::Symbol']]],
+  ['control_5fstyles_5',['CONTROL_STYLES',['../classLIEF_1_1PE_1_1ResourceDialog.html#a213a3726706ddfa613da7842113c485d',1,'LIEF::PE::ResourceDialog']]],
+  ['cpu_6',['CPU',['../classLIEF_1_1pdb_1_1BuildMetadata.html#a90719b727e681d248e026a468b6dd202',1,'LIEF::pdb::BuildMetadata']]],
+  ['cpu_5ftype_7',['CPU_TYPE',['../classLIEF_1_1MachO_1_1Header.html#ac6ba4ef6166df7dbfb54dcfee3157332',1,'LIEF::MachO::Header']]]
 ];
