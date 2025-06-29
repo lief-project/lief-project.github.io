46b72226cc4d27d1a1f3fdba99f2553f1cf77e7e
lief-ci-doc <lief-ci-doc@lief.re>
2025-06-29 18:49:10 +0000

    Update latest doc

--- doc/latest/doxygen/search/files_1.js
+++ doc/latest/doxygen/search/files_1.js
@@ -1,12 +1,15 @@
 var searchData=
 [
   ['base_2ehpp_0',['Base.hpp',['../Base_8hpp.html',1,'']]],
-  ['binaryparser_2ehpp_1',['BinaryParser.hpp',['../BinaryParser_8hpp.html',1,'']]],
-  ['binarystream_2ehpp_2',['BinaryStream.hpp',['../BinaryStream_8hpp.html',1,'']]],
-  ['bindinginfo_2ehpp_3',['BindingInfo.hpp',['../BindingInfo_8hpp.html',1,'']]],
-  ['bindinginfoiterator_2ehpp_4',['BindingInfoIterator.hpp',['../BindingInfoIterator_8hpp.html',1,'']]],
-  ['bitfield_2ehpp_5',['BitField.hpp',['../BitField_8hpp.html',1,'']]],
-  ['bpf_2edef_6',['BPF.def',['../BPF_8def.html',1,'']]],
-  ['buildtoolversion_2ehpp_7',['BuildToolVersion.hpp',['../BuildToolVersion_8hpp.html',1,'']]],
-  ['buildversion_2ehpp_8',['BuildVersion.hpp',['../BuildVersion_8hpp.html',1,'']]]
+  ['basetype_2ehpp_1',['BaseType.hpp',['../BaseType_8hpp.html',1,'']]],
+  ['bigobjheader_2ehpp_2',['BigObjHeader.hpp',['../BigObjHeader_8hpp.html',1,'']]],
+  ['binaryparser_2ehpp_3',['BinaryParser.hpp',['../BinaryParser_8hpp.html',1,'']]],
+  ['binarystream_2ehpp_4',['BinaryStream.hpp',['../BinaryStream_8hpp.html',1,'']]],
+  ['bindinginfo_2ehpp_5',['BindingInfo.hpp',['../BindingInfo_8hpp.html',1,'']]],
+  ['bindinginfoiterator_2ehpp_6',['BindingInfoIterator.hpp',['../BindingInfoIterator_8hpp.html',1,'']]],
+  ['bitfield_2ehpp_7',['BitField.hpp',['../BitField_8hpp.html',1,'']]],
+  ['bpf_2edef_8',['BPF.def',['../BPF_8def.html',1,'']]],
+  ['buildmetadata_2ehpp_9',['BuildMetadata.hpp',['../BuildMetadata_8hpp.html',1,'']]],
+  ['buildtoolversion_2ehpp_10',['BuildToolVersion.hpp',['../BuildToolVersion_8hpp.html',1,'']]],
+  ['buildversion_2ehpp_11',['BuildVersion.hpp',['../BuildVersion_8hpp.html',1,'']]]
 ];
--- doc/stable/doxygen/search/enums_4.js
+++ doc/stable/doxygen/search/enums_4.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['eh_5fencoding_0',['EH_ENCODING',['../namespaceLIEF_1_1dwarf.html#a160dd1eb57d44f7b8287f310a7256794',1,'LIEF::dwarf']]],
   ['elf_5fdata_1',['ELF_DATA',['../classLIEF_1_1ELF_1_1Header.html#a5b2de13f88453b37ef540c10b7c08bd5',1,'LIEF::ELF::Header']]],
-  ['elf_5ftype_2',['ELF_TYPE',['../classLIEF_1_1ELF_1_1Parser.html#adfc875b739437bcab73275344410fd8a',1,'LIEF::ELF::Parser']]],
-  ['encoding_3',['ENCODING',['../classLIEF_1_1dwarf_1_1types_1_1Base.html#a7a09fb1100ae0ff4635029152ef6ccd2',1,'LIEF::dwarf::types::Base::ENCODING'],['../classLIEF_1_1ELF_1_1Relocation.html#aef8bd5c5aa2b4df5b6e7b5ffc56154b9',1,'LIEF::ELF::Relocation::ENCODING']]],
-  ['endianness_4',['ENDIANNESS',['../namespaceLIEF.html#aeb2d22c6a3d6c904db8549c4410c9873',1,'LIEF']]],
-  ['extended_5fwindow_5fstyles_5',['EXTENDED_WINDOW_STYLES',['../namespaceLIEF_1_1PE.html#a30f4c7549b600db6beb9eee4e6bb4693',1,'LIEF::PE']]]
+  ['elf_5findent_2',['ELF_INDENT',['../classLIEF_1_1ELF_1_1Header.html#a9b05524e339856950cca9131ed4e8760',1,'LIEF::ELF::Header']]],
+  ['elf_5ftype_3',['ELF_TYPE',['../classLIEF_1_1ELF_1_1Parser.html#adfc875b739437bcab73275344410fd8a',1,'LIEF::ELF::Parser']]],
+  ['encoding_4',['ENCODING',['../classLIEF_1_1dwarf_1_1types_1_1Base.html#a7a09fb1100ae0ff4635029152ef6ccd2',1,'LIEF::dwarf::types::Base::ENCODING'],['../classLIEF_1_1ELF_1_1Relocation.html#aef8bd5c5aa2b4df5b6e7b5ffc56154b9',1,'LIEF::ELF::Relocation::ENCODING']]],
+  ['endianness_5',['ENDIANNESS',['../classLIEF_1_1Header.html#a6e62612028e6b3844bd2a1a313339297',1,'LIEF::Header']]],
+  ['extended_5fwindow_5fstyles_6',['EXTENDED_WINDOW_STYLES',['../namespaceLIEF_1_1PE.html#a30f4c7549b600db6beb9eee4e6bb4693',1,'LIEF::PE']]]
 ];
