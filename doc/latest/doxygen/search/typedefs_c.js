3629b808ebb8d36911dea9f7632abd0867f61193
lief-ci-doc <lief-ci-doc@lief.re>
2025-07-26 12:14:46 +0000

    Update latest doc

--- doc/latest/doxygen/search/typedefs_c.js
+++ doc/latest/doxygen/search/typedefs_c.js
@@ -1,4 +1,15 @@
 var searchData=
 [
-  ['notes_5ft_0',['notes_t',['../classLIEF_1_1ELF_1_1Binary.html#a844e34c57d0e123609961edd5a5fce16',1,'LIEF::ELF::Binary']]]
+  ['machine_5ftypes_0',['MACHINE_TYPES',['../classLIEF_1_1COFF_1_1Header.html#aca7f55eae6c76421c412bc42fb6c25cf',1,'LIEF::COFF::Header']]],
+  ['macho_5fbinary_5ft_1',['Macho_Binary_t',['../MachO_2Binary_8h.html#aa43da6dda26fe05e49addad8375ac11b',1,'Binary.h']]],
+  ['macho_5fcommand_5ft_2',['Macho_Command_t',['../LoadCommand_8h.html#a4abe41ecbce3c520959124f88baa63f8',1,'LoadCommand.h']]],
+  ['macho_5fheader_5ft_3',['Macho_Header_t',['../MachO_2Header_8h.html#a4c3821d90e0cb7057961b6ad28b13453',1,'Header.h']]],
+  ['macho_5fsection_5ft_4',['Macho_Section_t',['../MachO_2Section_8h.html#a55b431a6aeb54e7b629b7cbf00920b39',1,'Section.h']]],
+  ['macho_5fsegment_5ft_5',['Macho_Segment_t',['../MachO_2Segment_8h.html#a329f1b5d769a37b7297b91674fea8ffb',1,'Segment.h']]],
+  ['macho_5fsymbol_5ft_6',['Macho_Symbol_t',['../MachO_2Symbol_8h.html#abbd5ff3c31c27453e94b1f27c6355cd8',1,'Symbol.h']]],
+  ['magic_5ft_7',['magic_t',['../classLIEF_1_1ART_1_1Header.html#a51f7d3f19a6a754a85d6bf21c4b79390',1,'LIEF::ART::Header::magic_t'],['../classLIEF_1_1DEX_1_1Header.html#a87f86f201499d36b9a9c288199439547',1,'LIEF::DEX::Header::magic_t'],['../classLIEF_1_1OAT_1_1Header.html#a28e32276665ed6771008ce2068b4f324',1,'LIEF::OAT::Header::magic_t'],['../classLIEF_1_1VDEX_1_1Header.html#ae8136d80f150f2b35b96a3448002b37b',1,'LIEF::VDEX::Header::magic_t']]],
+  ['mapping_5finfo_5fiterator_8',['mapping_info_iterator',['../classLIEF_1_1dsc_1_1DyldSharedCache.html#a061518d23bb63dcdc28fd6c6cf2c11f7',1,'LIEF::dsc::DyldSharedCache']]],
+  ['methods_5fit_9',['methods_it',['../classLIEF_1_1objc_1_1Protocol.html#aac3ae9db56f1cd971f46f199a3f68f5b',1,'LIEF::objc::Protocol']]],
+  ['methods_5fiterator_10',['methods_iterator',['../classLIEF_1_1pdb_1_1types_1_1ClassLike.html#a1a4826aef2c759d56ae9921b81f4c421',1,'LIEF::pdb::types::ClassLike']]],
+  ['methods_5ft_11',['methods_t',['../classLIEF_1_1DEX_1_1Class.html#a982c0dcdb2289f0d25f1c14433c74992',1,'LIEF::DEX::Class::methods_t'],['../classLIEF_1_1DEX_1_1File.html#adc3d31f3cbb16630dece38c588f8d938',1,'LIEF::DEX::File::methods_t'],['../classLIEF_1_1OAT_1_1Binary.html#a8a40f23b86863b5d9043a0d62ec29be7',1,'LIEF::OAT::Binary::methods_t'],['../classLIEF_1_1OAT_1_1Class.html#a831a6ce22e1fb1c39340210341e2ea54',1,'LIEF::OAT::Class::methods_t'],['../classLIEF_1_1objc_1_1Class.html#a1fb84af8b57df65325c8c18555b35fed',1,'LIEF::objc::Class::methods_t']]]
 ];
